4d232a6cda8cf7cf8ea6b606a572103866b7278e
madjidsmail <madjidsmail1998@gmail.com>
2022-04-24 03:43:08 +0100

    first commit

---
+++ chaincode/chaincode/typescript/src/patient.ts
@@ -0,0 +1,32 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export class Patient {
+    public value: string;
+
+    // public patientID: string;
+    // public patientIDprivate: string;
+    public FirstName: string;
+    public FirstNameprivate: string;
+    public LastName: string;
+    public LastNameprivate: string;
+    public birthday: string;
+    public birthdayprivate: string;
+    public contact: string;
+    public contactprivate: string;
+    public address: string;
+    public addressprivate: string;
+    public bloodGroup: string;
+    public bloodGroupprivate: string;
+    public Allergies: string;
+    public Allergiesprivate: string;
+    public Diagnosis: string;
+    public Diagnosisprivate: string;
+    public treatment: string;
+    public treatmentprivate: string;
+    public lastVisits: string;
+    public lastVisitsprivate: string;
+    public doctorsWithpermission: string;
+    public doctorsWithpermissionprivate: string;
+}
